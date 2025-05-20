1715829ba763b472812e93e3b89bf521eeae8444
Marco <marcominisgallo@yahoo.it>
2025-05-20 14:35:15 +0200

    Class Evento ok

---
+++ src/main/java/Evento.java
@@ -0,0 +1,65 @@
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name= "eventi")
+public class Evento {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDate dataEvento;
+
+    private String descrizione;
+
+    @Enumerated(EnumType.STRING)
+    private TipoEvento tipoEvento;
+
+    private int numeroMassimoPartecipanti;
+
+    public Evento() {}
+
+    public Evento(LocalDate dataEvento, String descrizione, TipoEvento tipoEvento, int numeroMassimoPartecipanti) {
+        this.dataEvento = dataEvento;
+        this.descrizione = descrizione;
+        this.tipoEvento = tipoEvento;
+        this.numeroMassimoPartecipanti = numeroMassimoPartecipanti;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public LocalDate getDataEvento() {
+        return dataEvento;
+    }
+
+    public void setDataEvento(LocalDate dataEvento) {
+        this.dataEvento = dataEvento;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public TipoEvento getTipoEvento() {
+        return tipoEvento;
+    }
+
+    public void setTipoEvento(TipoEvento tipoEvento) {
+        this.tipoEvento = tipoEvento;
+    }
+
+    public int getNumeroMassimoPartecipanti() {
+        return numeroMassimoPartecipanti;
+    }
+
+    public void setNumeroMassimoPartecipanti(int numeroMassimoPartecipanti) {
+        this.numeroMassimoPartecipanti = numeroMassimoPartecipanti;
+    }
+}
